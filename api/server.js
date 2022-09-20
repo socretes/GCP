da19c62491e92cca8192c70d167ff0ec95f7b533
Andy Sharp <andysharp2003@yahoo.com>
2022-09-20 12:26:42 +0100

    8080

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express(),
       bodyParser = require("body-parser");
-      port = 3080;
+      port = 8080;
 
 // place holder for the data
 const users = [];
